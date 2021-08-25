770760a49b233d4083e41d9f3f2fbfd8ad852598
nekaartajaya <nekaartajaya@gmail.com>
2021-08-25 16:50:40 +0800

    updated app test js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Whos There/i);
+  const linkElement = screen.getByText(/This edited on feature branch/i);
   expect(linkElement).toBeInTheDocument();
 });
